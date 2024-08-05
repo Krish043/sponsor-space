495252d39251fd76c2d8dd0dd367b0fdd4ae5fb6
Krish <159705912+Krish043@users.noreply.github.com>
2024-08-05 18:47:08 +0530

    Update page.js

--- app/ShippingPolicy/page.js
+++ app/ShippingPolicy/page.js
@@ -5,7 +5,7 @@ const ShippingPolicy = () => {
     <div className="m-12 p-8 rounded-lg shadow-md max-w-4xl mx-auto mt-10">
       <h1 className="text-3xl font-bold text-slate-400 mb-6">Shipping Policy</h1>
       
-      <p className="text-white mb-4">Welcome to SponsorSpace's Shipping Policy!</p>
+      <p className="text-white mb-4">Welcome to SponsorSpace&apos;s Shipping Policy!</p>
       <p className="text-white mb-6">
         This Shipping Policy applies to any physical products or merchandise offered on our platform. Please read the following information carefully to understand our policies regarding shipping and delivery.
       </p>
@@ -37,7 +37,7 @@ const ShippingPolicy = () => {
 
       <h2 className="text-2xl font-semibold text-slate-400 mb-4">5. Shipping Restrictions</h2>
       <p className="text-white mb-6">
-        We currently ship to addresses within [Country/Region]. We do not ship to P.O. Boxes or military addresses (APO/FPO).
+        We currently ship to addresses within India. We do not ship to P.O. Boxes or military addresses (APO/FPO).
       </p>
 
       <h2 className="text-2xl font-semibold text-slate-400 mb-4">6. Customs, Duties, and Taxes</h2>
@@ -52,12 +52,12 @@ const ShippingPolicy = () => {
 
       <h2 className="text-2xl font-semibold text-slate-400 mb-4">8. International Shipping Policy</h2>
       <p className="text-white mb-6">
-        We currently do not ship outside [Country/Region]. Any changes to our international shipping policy will be updated here.
+        We currently do not ship outside India. Any changes to our international shipping policy will be updated here.
       </p>
 
       <h2 className="text-2xl font-semibold text-slate-400 mb-4">9. Contact Us</h2>
       <p className="text-white mb-6">
-        If you have any questions or concerns about this Shipping Policy, please contact us at <a href="mailto:support@sponsorspace.com" className="text-blue-400 underline">support@sponsorspace.com</a>.
+        If you have any questions or concerns about this Shipping Policy, please contact us at <a href="mailto:krisjchetangangajaliya@gmail.com" className="text-blue-400 underline">krisjchetangangajaliya@gmail.com</a>.
       </p>
 
       <p className="text-white">
