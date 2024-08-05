b8eb0fe8e24432e3debf861eaf39c0abd4ffee95
Krish <159705912+Krish043@users.noreply.github.com>
2024-08-05 23:37:46 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,15 +1,40 @@
-"use client";
-import React from "react";
+// "use client";
+// import React from "react";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+// import { useSession, signIn, signOut } from "next-auth/react"
+// import { useRouter } from "next/navigation";
+
+// const Dashboard = () => {
+//   const { data: session } = useSession();
+//   if (!session) {
+//     const router = useRouter();
+//     router.push("/login");
+//   }
+//   return <div>dashboard</div>;
+// };
+
+// export default Dashboard;
+
+
+"use client";
+import React, { useEffect } from "react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push("/login");
+    }
+  }, [session, router]);
+
   if (!session) {
-    const router = useRouter();
-    router.push("/login");
+    return null; // or you can return a loading spinner or placeholder
   }
+
   return <div>dashboard</div>;
 };
 
